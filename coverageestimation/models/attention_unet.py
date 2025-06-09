8c248c20ef87953113b998205bcc57e8c43efd12
KareemElozeiri <k.elozeiri@gmail.com>
2025-06-09 22:14:53 +0300

    fix(attention_unet): Apply memory reduction

--- coverageestimation/models/attention_unet.py
+++ coverageestimation/models/attention_unet.py
@@ -8,14 +8,15 @@ class ConvBlock(nn.Module):
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
         self.relu = nn.ReLU(inplace=True)
-        self.batch_norm = nn.BatchNorm2d(out_channels)
+        self.batch_norm1 = nn.BatchNorm2d(out_channels)
+        self.batch_norm2 = nn.BatchNorm2d(out_channels)
 
     def forward(self, x):
         x = self.conv1(x)
-        x = self.batch_norm(x)
+        x = self.batch_norm1(x)
         x = self.relu(x)
         x = self.conv2(x)
-        x = self.batch_norm(x)
+        x = self.batch_norm2(x)
         x = self.relu(x)
         return x
 
@@ -58,31 +59,35 @@ class UpConv(nn.Module):
     
     
 class AttentionUNet(BaseTensorCNN):
-    def __init__(self, input_channels=2, output_channels=1):
+    def __init__(self, input_channels=2, output_channels=1, base_channels=32):
+        # Reduce base channels to save memory
+        self.base_channels = base_channels
         super(AttentionUNet, self).__init__(input_channels, output_channels)
 
     def _create_model(self):
-        # Return nn.ModuleDict directly, not wrapped in nn.Sequential
+        # Use smaller channel counts to reduce memory usage
+        bc = self.base_channels  # base channels
+        
         return nn.ModuleDict({
-            'encoder1': ConvBlock(self.input_channels, 64),
-            'encoder2': ConvBlock(64, 128),
-            'encoder3': ConvBlock(128, 256),
-            'encoder4': ConvBlock(256, 512),
+            'encoder1': ConvBlock(self.input_channels, bc),
+            'encoder2': ConvBlock(bc, bc*2),
+            'encoder3': ConvBlock(bc*2, bc*4),
+            'encoder4': ConvBlock(bc*4, bc*8),
             'pool': nn.MaxPool2d(kernel_size=2, stride=2),
-            'bottleneck': ConvBlock(512, 1024),
-            'upconv4': UpConv(1024, 512),
-            'attention4': AttentionBlock(F_g=512, F_l=512, F_int=256),
-            'decoder4': ConvBlock(1024, 512),
-            'upconv3': UpConv(512, 256),
-            'attention3': AttentionBlock(F_g=256, F_l=256, F_int=128),
-            'decoder3': ConvBlock(512, 256),
-            'upconv2': UpConv(256, 128),
-            'attention2': AttentionBlock(F_g=128, F_l=128, F_int=64),
-            'decoder2': ConvBlock(256, 128),
-            'upconv1': UpConv(128, 64),
-            'attention1': AttentionBlock(F_g=64, F_l=64, F_int=32),
-            'decoder1': ConvBlock(128, 64),
-            'final_conv': nn.Conv2d(64, self.output_channels, kernel_size=1, stride=1, padding=0)
+            'bottleneck': ConvBlock(bc*8, bc*16),
+            'upconv4': UpConv(bc*16, bc*8),
+            'attention4': AttentionBlock(F_g=bc*8, F_l=bc*8, F_int=bc*4),
+            'decoder4': ConvBlock(bc*16, bc*8),
+            'upconv3': UpConv(bc*8, bc*4),
+            'attention3': AttentionBlock(F_g=bc*4, F_l=bc*4, F_int=bc*2),
+            'decoder3': ConvBlock(bc*8, bc*4),
+            'upconv2': UpConv(bc*4, bc*2),
+            'attention2': AttentionBlock(F_g=bc*2, F_l=bc*2, F_int=bc),
+            'decoder2': ConvBlock(bc*4, bc*2),
+            'upconv1': UpConv(bc*2, bc),
+            'attention1': AttentionBlock(F_g=bc, F_l=bc, F_int=bc//2),
+            'decoder1': ConvBlock(bc*2, bc),
+            'final_conv': nn.Conv2d(bc, self.output_channels, kernel_size=1, stride=1, padding=0)
         })
 
     def forward(self, x):
@@ -95,20 +100,28 @@ class AttentionUNet(BaseTensorCNN):
         # Bottleneck
         bottleneck = self.model['bottleneck'](self.model['pool'](enc4))
 
-        # Decoder
+        # Decoder with gradient checkpointing for memory efficiency
         dec4 = self.model['upconv4'](bottleneck)
+        # Clear bottleneck from memory
+        del bottleneck
         dec4 = torch.cat((self.model['attention4'](dec4, enc4), dec4), dim=1)
         dec4 = self.model['decoder4'](dec4)
 
         dec3 = self.model['upconv3'](dec4)
+        # Clear enc4 from memory
+        del enc4
         dec3 = torch.cat((self.model['attention3'](dec3, enc3), dec3), dim=1)
         dec3 = self.model['decoder3'](dec3)
 
         dec2 = self.model['upconv2'](dec3)
+        # Clear enc3 from memory
+        del enc3
         dec2 = torch.cat((self.model['attention2'](dec2, enc2), dec2), dim=1)
         dec2 = self.model['decoder2'](dec2)
 
         dec1 = self.model['upconv1'](dec2)
+        # Clear enc2 from memory
+        del enc2
         dec1 = torch.cat((self.model['attention1'](dec1, enc1), dec1), dim=1)
         dec1 = self.model['decoder1'](dec1)
 
@@ -117,4 +130,4 @@ class AttentionUNet(BaseTensorCNN):
         return out
     
     def get_model_name(self):
-        return "AttentionUNet"
+        return f"AttentionUNet_bc{self.base_channels}"
